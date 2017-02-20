77dc49c5e2634c204210f743ca6629d47e71a35d
Vincent Rodomista <vrodom1@lsu.edu>
2017-02-19 23:42:01 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function playSound(){
+	var audio = new Audio('fox00.dsp.wav');
+	audio.play();
+}
+
+function playSound1(){
+	var audio = new Audio('fox04.dsp.wav');
+	audio.play();
+}
+
+function playSound2(){
+	var audio = new Audio('fox0a.dsp.wav');
+	audio.play();
+}
+
+function playSound3(){
+	var audio = new Audio('fox0d.dsp.wav');
+	audio.play();
+}
+
+
+
